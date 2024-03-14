f2715c61c5e9a0860737d26879ac5c5a6744c7dd
Alexander Linse <zandervon24@gmail.com>
2024-03-14 15:44:10 -0500

    Added some freelancers

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Link check");
+const freelancers = [
+  { name: "Alice", occupation: "Writer", price: 30, },
+  { name: "Bob", occupation: "Teacher", price: 50, },
+  { name: "Carol", occupation: "Programmer", price: 70, },
+  { name: "Jack", occupation: "Musician", price: 20, },
+  { name: "Mike", occupation: "Painter", price: 25, },
+];
+
+console.log(freelancers);
